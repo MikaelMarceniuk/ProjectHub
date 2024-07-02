851a100c8fc7f7ef95664e9a24f0bf46d321f68e
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-07-02 14:33:19 -0300

    improve(getProjectByUser): Implement search using case insensitive

--- src/api/getProjectByUser.ts
+++ src/api/getProjectByUser.ts
@@ -2,7 +2,7 @@
 
 import db from '@/db'
 import { ProjectSchema } from '@/db/schema'
-import { and, eq, like } from 'drizzle-orm'
+import { and, eq, ilike } from 'drizzle-orm'
 
 type getProjectByUserParams = {
 	userId: string
@@ -11,15 +11,13 @@ type getProjectByUserParams = {
 
 const getProjectByUser = async ({ userId, query }: getProjectByUserParams) => {
 	try {
-		// TODO Fix userId not in session after f5
-		// TODO Make filter by lower project name https://orm.drizzle.team/learn/guides/unique-case-insensitive-email#postgresql
 		const dbResp = await db
 			.select()
 			.from(ProjectSchema)
 			.where(
 				and(
 					eq(ProjectSchema.ownerId, userId),
-					like(ProjectSchema.name, `%${query || ''}%`),
+					ilike(ProjectSchema.name, `%${query || ''}%`),
 				),
 			)
 
