4e3f2d099892de97aaa1023dcf3d7b7edfbca63e
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-07-04 06:59:36 -0300

    improve(searchFormProvider): Implement url update

--- src/app/(app)/dashboard/_providers/searchFormProvider.tsx
+++ src/app/(app)/dashboard/_providers/searchFormProvider.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import withChildren from '@/@types/withChildren'
@@ -16,21 +16,25 @@ export type searchFormType = z.infer<typeof searchSchema>
 const SearchFormProvider: React.FC<withChildren> = ({ children }) => {
 	const pathname = usePathname()
 	const router = useRouter()
+	const searchParams = useSearchParams()
 
 	const form = useForm<searchFormType>({
 		resolver: zodResolver(searchSchema),
 		defaultValues: {
 			query: '',
 		},
+		values: {
+			query: searchParams.get('query') || '',
+		},
 	})
 
 	const queryValue = form.watch('query')
 
 	useEffect(() => {
-		const params = new URLSearchParams(form.toString())
+		const params = new URLSearchParams()
 		params.set('query', queryValue)
 
-		// router.push(pathname + '?' + params.toString())
+		router.push(pathname + '?' + params.toString())
 	}, [queryValue])
 
 	return <FormProvider {...form}>{children}</FormProvider>
